7434b4447a523bb1940c86a54d2ba2c6f6b8b394
Karan Krishnani <krkrishn@thoughtworks.com>
2018-10-16 13:25:50 -0400

    section 1 code with prints

---
+++ src/text_classification.py
@@ -0,0 +1,54 @@
+from sklearn import model_selection, preprocessing, linear_model, naive_bayes, metrics, svm
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn import decomposition, ensemble
+
+import pandas, xgboost, numpy, textblob, string
+from keras.preprocessing import text, sequence
+from keras import layers, models, optimizers
+
+# load the dataset
+# sample data
+# __label__2 an absolute masterpiece: I am quite sure any of you actually taking the time to read this have played the game at least once, and heard at least a few of the tracks here. And whether you were aware of it or not, Mitsuda's music contributed greatly to the mood of every single minute of the whole game.Composed of 3 CDs and quite a few songs (I haven't an exact count), all of which are heart-rendering and impressively remarkable, this soundtrack is one I assure you you will not forget. It has everything for every listener -- from fast-paced and energetic (Dancing the Tokage or Termina Home), to slower and more haunting (Dragon God), to purely beautifully composed (Time's Scar), to even some fantastic vocals (Radical Dreamers).This is one of the best videogame soundtracks out there, and surely Mitsuda's best ever. ^_^
+# __label__1 Buyer beware: This is a self-published book, and if you want to know why--read a few paragraphs! Those 5 star reviews must have been written by Ms. Haddon's family and friends--or perhaps, by herself! I can't imagine anyone reading the whole thing--I spent an evening with the book and a friend and we were in hysterics reading bits and pieces of it to one another. It is most definitely bad enough to be entered into some kind of a "worst book" contest. I can't believe Amazon even sells this kind of thing. Maybe I can offer them my 8th grade term paper on "To Kill a Mockingbird"--a book I am quite sure Ms. Haddon never heard of. Anyway, unless you are in a mood to send a book to someone as a joke---stay far, far away from this one!
+data = open('data/corpus').read()
+labels, texts = [], []
+for i, line in enumerate(data.split('\n')):
+    content = line.split()
+    labels.append(content[0])
+    texts.append(content[1:])
+
+print(f'Raw Split Data')
+print(f'*******')
+print(labels[0:2], texts[0:2])
+print(f'*******')
+
+# create a data frame using texts and labels
+trainDF = pandas.DataFrame()
+trainDF['text'] = texts
+trainDF['label'] = labels
+
+print(f'Data Frame')
+print(f'*******')
+print(trainDF.values)
+print(f'*******')
+
+# split the dataset into training and validation datasets 
+train_x, valid_x, train_y, valid_y = model_selection.train_test_split(trainDF['text'], trainDF['label'])
+print(f'Train X: {train_x}')
+print(f'*******')
+print(f'Valid X: {valid_x}')
+print(f'*******')
+print(f'Train Y: {train_y}')
+print(f'*******')
+print(f'Valid Y: {valid_y}')
+
+# label encode the target variable 
+encoder = preprocessing.LabelEncoder()
+train_y = encoder.fit_transform(train_y)
+valid_y = encoder.fit_transform(valid_y)
+
+print(f'Label Encoding')
+print(f'*******')
+print(f'Labels Encoded Train Y: {train_y}')
+print(f'Labels Encoded Valid Y: {valid_y}')
+print(f'*******')
