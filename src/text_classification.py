0d880e92de293005e49154bf5a01d669f32eccf1
Karan Krishnani <krkrishn@thoughtworks.com>
2018-10-19 11:23:58 -0400

    [KK] adds argparser so we can add multiple feature options

--- src/text_classification.py
+++ src/text_classification.py
@@ -6,6 +6,14 @@ import pandas, xgboost, numpy, textblob, string
 from keras.preprocessing import text, sequence
 from keras import layers, models, optimizers
 
+import argparse
+
+parser = argparse.ArgumentParser(description='Text Classification.')
+parser.add_argument('--verbose', action='store_true',
+                   help='displays debug logging')
+
+args = parser.parse_args()
+
 # load the dataset
 # sample data
 # __label__2 an absolute masterpiece: I am quite sure any of you actually taking the time to read this have played the game at least once, and heard at least a few of the tracks here. And whether you were aware of it or not, Mitsuda's music contributed greatly to the mood of every single minute of the whole game.Composed of 3 CDs and quite a few songs (I haven't an exact count), all of which are heart-rendering and impressively remarkable, this soundtrack is one I assure you you will not forget. It has everything for every listener -- from fast-paced and energetic (Dancing the Tokage or Termina Home), to slower and more haunting (Dragon God), to purely beautifully composed (Time's Scar), to even some fantastic vocals (Radical Dreamers).This is one of the best videogame soundtracks out there, and surely Mitsuda's best ever. ^_^
@@ -16,39 +24,42 @@ for i, line in enumerate(data.split('\n')):
     content = line.split()
     labels.append(content[0])
     texts.append(content[1:])
-
-print(f'Raw Split Data')
-print(f'*******')
-print(labels[0:2], texts[0:2])
-print(f'*******')
+if args.verbose:
+    print(f'Raw Split Data')
+    print(f'*******')
+    print(labels[0:2], texts[0:2])
+    print(f'*******')
 
 # create a data frame using texts and labels
 trainDF = pandas.DataFrame()
 trainDF['text'] = texts
 trainDF['label'] = labels
 
-print(f'Data Frame')
-print(f'*******')
-print(trainDF.values)
-print(f'*******')
+if args.verbose:
+    print(f'Data Frame')
+    print(f'*******')
+    print(trainDF.values)
+    print(f'*******')
 
 # split the dataset into training and validation datasets 
 train_x, valid_x, train_y, valid_y = model_selection.train_test_split(trainDF['text'], trainDF['label'])
-print(f'Train X: {train_x}')
-print(f'*******')
-print(f'Valid X: {valid_x}')
-print(f'*******')
-print(f'Train Y: {train_y}')
-print(f'*******')
-print(f'Valid Y: {valid_y}')
+if args.verbose:
+    print(f'Train X: {train_x}')
+    print(f'*******')
+    print(f'Valid X: {valid_x}')
+    print(f'*******')
+    print(f'Train Y: {train_y}')
+    print(f'*******')
+    print(f'Valid Y: {valid_y}')
 
 # label encode the target variable 
 encoder = preprocessing.LabelEncoder()
 train_y = encoder.fit_transform(train_y)
 valid_y = encoder.fit_transform(valid_y)
 
-print(f'Label Encoding')
-print(f'*******')
-print(f'Labels Encoded Train Y: {train_y}')
-print(f'Labels Encoded Valid Y: {valid_y}')
-print(f'*******')
+if args.verbose:
+    print(f'Label Encoding')
+    print(f'*******')
+    print(f'Labels Encoded Train Y: {train_y}')
+    print(f'Labels Encoded Valid Y: {valid_y}')
+    print(f'*******')
